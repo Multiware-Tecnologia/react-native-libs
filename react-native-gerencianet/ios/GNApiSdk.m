9e62789ca6ebccd148b227e450b6e2d037578623
joao <jvd.ramos@hotmail.com>
2022-05-18 16:52:43 -0400

    update: atualizado podfile

--- react-native-gerencianet/ios/GNApiSdk.m
+++ react-native-gerencianet/ios/GNApiSdk.m
@@ -13,7 +13,7 @@ RCT_EXPORT_MODULE()
 // Example method
 // See // https://reactnative.dev/docs/native-modules-ios
 
-  RCT_EXPORT_METHOD(generateHash: (NSString *)number :(NSString *)brand :(NSString *)ccv :(NSString *)month :(NSString *)year :(NSString *)accountId :(Bool *)approvalMode :(RCTResponseSenderBlock)callback)
+  RCT_EXPORT_METHOD(generateHash: (NSString *)number :(NSString *)brand :(NSString *)ccv :(NSString *)month :(NSString *)year :(NSString *)accountId :(Boolean *)approvalMode :(RCTResponseSenderBlock)callback)
 {
   GNConfig *gnConfig = [[GNConfig alloc] initWithAccountCode:accountId sandbox:approvalMode];
   GNApiEndpoints *gnApi = [[GNApiEndpoints alloc] initWithConfig:gnConfig];
@@ -28,8 +28,8 @@ RCT_EXPORT_MODULE()
     [gnApi paymentTokenForCreditCard:creditCard]
     .then(^(GNPaymentToken *paymentToken){
     NSLog(@"%@", paymentToken.token);
-    NSString *events =[rn generateHashWithCardNumber:number cardBrand:brand securityCode:ccv expirationMonth:expirationMonth expirationYear:expirationYear ]
-    callback(@[[NSNull null], events]);
+    // NSString *events =[rn generateHashWithCardNumber:number cardBrand:brand securityCode:ccv expirationMonth:expirationMonth expirationYear:expirationYear ]
+    callback(@[[NSNull null], paymentToken.token]);
     })
     .catch(^(GNError *error){
     NSLog(@"An error occurred: %@", error.message);
